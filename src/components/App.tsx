dddd4bf2aafa76d5dc053d40bd6b185d990d1d6b
Aleksei Khokhlov <lexasby@gmail.com>
2025-01-22 14:27:10 +0100

    add react page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
-  return <div>Hello Dude</div>;
+  const [count, setCount] = useState<number>(0);
+  const increment = () => setCount((prev) => prev + 1);
+  return (
+    <>
+      <div>Hello Dude</div>
+      <h1>{count}</h1>
+      <button onClick={increment}></button>
+    </>
+  );
 };
