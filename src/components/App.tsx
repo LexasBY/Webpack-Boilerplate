70fb368271f269b2b657be0854fcbe32f8be6644
Aleksei Khokhlov <lexasby@gmail.com>
2025-01-23 11:07:20 +0100

    refactoring

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import "./App.scss";
 
 export const App = () => {
   const [count, setCount] = useState<number>(0);
   const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
   return (
     <>
-      <div>Hello Dude</div>
+      <div>Hello Dude!</div>
       <h1>{count}</h1>
-      <button onClick={increment}></button>
+      <button onClick={decrement}>Touch me!</button>
+      <button onClick={increment}>Push me!</button>
     </>
   );
 };
